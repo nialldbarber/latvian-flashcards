5412c91974030fd20222cfe95b011b2f13ae1cd5
Niall <nialldbarber@gmail.com>
2019-07-09 07:11:26 +0100

    Initial commit

---
+++ src/components/answer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Answer() {
+  return (
+    <div>
+      <p>Čau</p>
+      <input type="text" />
+    </div>
+  );
+}
---
+++ src/components/flashcard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Question from 'components/question';
+import Answer from 'components/answer';
+
+export default function Flashcard() {
+  return (
+    <div>
+      <Question />
+      <Answer />
+    </div>
+  );
+}
